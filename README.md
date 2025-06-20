ca4a66f8fef29b6c22e7ed3c7c34c1dbe2731e1f
FoxMoss <foxmoss@mediaology.com>
2025-06-20 18:11:31 -0500

    fix: cleanup git repo

---
+++ README.md
@@ -0,0 +1,16 @@
+# LibT9
+A C library for creating [T9](https://en.wikipedia.org/wiki/T9_%28predictive_text%29) typing systems.
+
+## Dependencies
+None! The library requires nothing but a basic implementation of the C standard library. 
+
+## Todo
+Feel free to contribute!
+
+- [ ] Punctuation
+- [ ] IBus Driver
+- [ ] Support for non-words
+
+## Credit
+The image used as the background for the web demo was created by Matt Hardy.     
+Word data set is from [Takala on HuggingFace](https://huggingface.co/datasets/takala/financial_phrasebank)
---
+++ docs/script.js
@@ -0,0 +1,40 @@
+let get_word = (path, index) => {
+  let ptr = _malloc(path.length + 1);
+  stringToUTF8(path, ptr, path.length + 1)
+  let res = UTF8ToString(_get_word(ptr, index))
+  _free(ptr);
+  return res;
+}
+
+let str = "";
+let current_word = [];
+let index = 0;
+
+let text_box = document.getElementById("text");
+let rerender = () => {
+  text_box.value = str + get_word(current_word.join(""), index);
+}
+
+for (let key = 1; key <= 9; key++) {
+  document.getElementById(key.toString()).addEventListener("click", () => {
+    current_word.push(key.toString());
+    rerender();
+    index = 0;
+  });
+}
+document.getElementById("*").addEventListener("click", () => {
+  index++;
+  rerender();
+});
+
+document.getElementById("0").addEventListener("click", () => {
+  str += get_word(current_word.join(""), index) + " ";
+  current_word = [];
+  index = 0;
+});
+
+document.getElementById("#").addEventListener("click", () => {
+  current_word.pop();
+  rerender();
+  index = 0;
+});
